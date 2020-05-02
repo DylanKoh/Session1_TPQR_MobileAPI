7acf8afe48cc0e37015a90709c6e6e2ee40b3402
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-05-02 22:52:39 +0800

    Controller ready for 1.2 and 1.3

---
+++ Controllers/User_TypeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Session1_TPQR_MobileAPI;
+
+namespace Session1_TPQR_MobileAPI.Controllers
+{
+    public class User_TypeController : Controller
+    {
+        private Session1Entities db = new Session1Entities();
+
+        public User_TypeController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        [HttpPost]
+        // POST: User_Type
+        public ActionResult Index()
+        {
+            return new JsonResult { Data = db.User_Type.ToList() };
+        }
+
+        
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
